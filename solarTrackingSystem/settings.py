77ba9826b484445b5f1c11e7e676ac1b69ccfcd3
karankbinu799 <opensourceitems@gmail.com>
2024-02-28 08:31:52 +0530

    latest commit

--- solarTrackingSystem/settings.py
+++ solarTrackingSystem/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-cj62*r)@nrm(z%a8bs)&9cf0e)0#it6ndqt7h4+55_p3xmt5i!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
@@ -132,7 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
