abee48ba99b821560afae94b40bc3caebc2572ce
karankbinu799 <opensourceitems@gmail.com>
2024-02-28 08:16:55 +0530

    latest commit

--- solarTrackingSystem/settings.py
+++ solarTrackingSystem/settings.py
@@ -132,8 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
 
 # email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
