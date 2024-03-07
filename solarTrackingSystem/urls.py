f6dc59c8154c96d56ec0e2be08f2f116dd67c2ec
karankbinu799 <opensourceitems@gmail.com>
2024-03-07 08:06:23 +0530

    latest

--- solarTrackingSystem/urls.py
+++ solarTrackingSystem/urls.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from SolarTrackingSystem import solarTrackingApp
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("solarTrackingApp.urls")),
 ]
+# Add static files serving for development only
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Catch-all pattern for 404 page
+urlpatterns += [
+    path("<path:unknown_path>", solarTrackingApp.views.custom_404, name="custom_404"),
+]
